02975092e247606fc97fb7135743804ba1c62ac8
王伟杭 <1260050422@qq.com>
2022-06-07 17:58:54 +0800

    修复点赞列表

--- controller/favorite.go
+++ controller/favorite.go
@@ -6,10 +6,10 @@ import (
 	"github.com/RaymondCode/simple-demo/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type FavoriteQuery struct {
-	UserId     int64  `form:"user_id" binding:"required"`
 	Token      string `form:"token" binding:"required"`
 	VideoId    int64  `form:"video_id" binding:"required"`
 	ActionType int    `form:"actionType" binding:"required"`
@@ -29,38 +29,34 @@ var favoriteService = service.NewFavoriteService()
 
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
-	var favoriteQuery FavoriteQuery
-	err := c.ShouldBindQuery(&favoriteQuery)
-	if err != nil {
-		c.JSON(http.StatusForbidden, model.Response{StatusCode: 403, StatusMsg: "参数不合法"})
-		return
-	}
-	if user := db.Redis.Get(favoriteQuery.Token); user == nil {
+	token := c.Query("token")
+	videoId, _ := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	actionType, _ := strconv.Atoi(c.Query("action_type"))
+	if user := db.Redis.Get(token); user == nil {
 		c.JSON(http.StatusForbidden, model.Response{StatusCode: 403, StatusMsg: "用户未登录！"})
-		return
 	}
-	if favoriteQuery.ActionType == 1 {
-		favoriteService.DoLike(favoriteQuery.UserId, favoriteQuery.VideoId)
-	} else if favoriteQuery.ActionType == 2 {
-		favoriteService.CancelLike(favoriteQuery.UserId, favoriteQuery.VideoId)
+	loginUser := service.NewUserService().FindUserByToken(token)
+	if actionType == 1 {
+		favoriteService.DoLike(loginUser.Id, videoId)
+		c.JSON(http.StatusOK, model.Response{StatusCode: 200, StatusMsg: "点赞成功"})
+	} else if actionType == 2 {
+		favoriteService.CancelLike(loginUser.Id, videoId)
+		c.JSON(http.StatusOK, model.Response{StatusCode: 200, StatusMsg: "取消点赞成功"})
 	} else {
 		c.JSON(http.StatusBadRequest, model.Response{StatusCode: 400, StatusMsg: "未知错误"})
 	}
+
 }
 
 // FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
-	var favoriteListQuery FavoriteListQuery
-	err := c.ShouldBindQuery(&favoriteListQuery)
-	if err != nil {
-		c.JSON(http.StatusForbidden, model.Response{StatusCode: 403, StatusMsg: "参数不合法"})
-		return
-	}
-	if user := db.Redis.Get(favoriteListQuery.Token); user == nil {
+	token := c.Query("token")
+	loginUser := service.NewUserService().FindUserByToken(token)
+	if user := db.Redis.Get(token); user == nil {
 		c.JSON(http.StatusForbidden, model.Response{StatusCode: 403, StatusMsg: "用户未登录！"})
 		return
 	}
-	likeVideoList := favoriteService.GetLikeList(favoriteListQuery.UserId)
+	likeVideoList := favoriteService.GetLikeList(loginUser.Id)
 	c.JSON(http.StatusOK, FavoriteListResponse{
 		Response: model.Response{
 			StatusCode: 200,
